635d706d0fcd72347fdaca656f5ad8532b8041cc
Josh Silverstein <silverjosh90@gmail.com>
2016-05-19 22:28:22 -0600

    Oauth working

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,16 +10,32 @@
  */
 var app = angular.module('tattiFrontEndApp', [
     'ngAnimate',
-    'ngRoute'
+    'ngRoute',
+    'satellizer'
   ])
-  app.config(function ($routeProvider) {
+  app.config(['$routeProvider', '$authProvider', function ($routeProvider, $authProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
+      .when('/:id', {
+        templateUrl: 'views/profile.html',
+        controller: 'profileController',
+        controllerAs: 'main'
+      })
       .otherwise({
         redirectTo: '/'
       });
-  });
+
+    $authProvider
+        .facebook({
+        clientId: '1716247038662898',
+        url: 'http://localhost:3000/auth/facebook',
+        scope: ['email'],
+        scopeDelimiter: ',',
+        profileFields: ['name', 'id', 'picture.type(large)', 'emails']
+      });
+
+  }]);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,22 @@
  * Controller of the tattiFrontEndApp
  */
 
-  app.controller('MainCtrl', ['$scope', function ($scope) {
+  app.controller('MainCtrl', ['$scope', '$auth', function ($scope, $auth) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    $scope.authenticate = function(provider) {
+      console.log($auth);
+      $auth.authenticate(provider).then(function(results){
+        console.log(results.data[0]);
+        window.location = '/#/'+ results.data[0].fb_id;
+      })
+  };
+
+  }]);
+  app.controller('profileController', ['$scope', function ($scope) {
+    console.log('YOLO')
+
   }]);
